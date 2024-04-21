8adfcd3f1029281947316c98cb985bd1f0b6d78a
EmLiu0314 <emilyliu52106@gmail.com>
2024-04-21 01:29:44 -0500

    updates

---
+++ TravelAppMidterm/FileUploader.swift
@@ -0,0 +1,18 @@
+//
+//  FileUploader.swift
+//  TravelAppMidterm
+//
+//  Created by Liu, Emily on 4/19/24.
+//
+
+import SwiftUI
+
+struct FileUploader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FileUploader()
+}
